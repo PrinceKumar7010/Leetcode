79c5399ddf0456766957797285dc5c2e1ee19d42
Prince Kumar <115650778+PrinceKumar7010@users.noreply.github.com>
2024-01-04 22:23:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
+++ Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -10,24 +10,19 @@ using namespace std;
 
 class Solution {
   public:
-    int singleElement(int arr[] ,int N) {
-         sort(arr,arr+N);
-        for(int i=0;i<N;i++)
-        {
-            if((i==0) && (arr[i]!=arr[i+1]))
-            {
-                return arr[i];
-            }
-            else if((i==N-1) && (arr[i] != arr[N-2]))
-            {
-                return arr[i];
-            }
-            else if((arr[i] != arr[i-1]) && (arr[i] != arr[i+1]))
-            {
-                return arr[i];
-            }
-        }
-        return -1;
+    int singleElement(int arr[] ,int n) {
+        
+    sort(arr,arr+n);
+    for(int i=0;i<n;i+=3){
+        
+        if(arr[i+1]!=arr[i])return arr[i];
+    }
+    return -1;
+        
+        
+        
+        
+        
     }
 };
 
