8af35712cfb966f013bd52dc458b72f81c1e085b
Prince Kumar <115650778+PrinceKumar7010@users.noreply.github.com>
2024-01-31 10:34:52 +0530

    Time: 128 ms (81.51%) | Memory: 100.9 MB (68.65%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-        vector<int> v;
         int n = t.size();
-        stack<int> st;
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && t[st.top()] <= t[i]) {
-                st.pop();
-            }
-
-            if (st.empty()) {
-                v.push_back(0);  // No warmer day found
-            } else {
-                v.push_back(st.top() - i);  // Calculate the distance to the warmer day
-            }
-
-            st.push(i);
+        stack<pair<int,int>> s;
+        vector<int> ans(n,0);
+    
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!s.empty() && t[i] >= s.top().first) s.pop();
+            if(!s.empty()) ans[i] = s.top().second-i;
+            s.push({t[i],i});
         }
-
-        reverse(v.begin(), v.end());  // Reverse the result to have it in the correct order
-        return v;
+        return ans;
     }
 };
