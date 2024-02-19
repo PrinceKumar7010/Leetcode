17b10bf7ff7a22ab88fa439825175c5c9b0c54b7
Prince Kumar <115650778+PrinceKumar7010@users.noreply.github.com>
2024-02-19 11:31:45 +0530

    Time: 53 ms (19.65%) | Memory: 46.8 MB (17.48%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    void func(int idx, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-        if (idx == arr.size()) {
-            if (target == 0) {
-                ans.push_back(ds);
-            }
-            return;
-        }
-        if (arr[idx] <= target) {
-            ds.push_back(arr[idx]);
-            func(idx, target - arr[idx], arr, ans, ds);
-            ds.pop_back();
-        }
-        func(idx + 1, target, arr, ans, ds);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        func(0, target, candidates, ans, ds);
-        return ans;
+
+
+   void sub( vector<vector<int>> &ans,vector<int> &v,int n,int i,int tar,vector<int>can){
+
+   if(i==n){
+if(tar==0){
+     ans.push_back(v);
+   
+   }
+       return ;
+   }
+   
+   
+   
+   if(can[i]<=tar){
+       v.push_back(can[i]);
+    sub(ans,v,n,i,tar-can[i],can);
+    v.pop_back();
+   }
+   sub(ans,v,n,i+1,tar,can);
+    
+
+
+   }
+    vector<vector<int>> combinationSum(vector<int>& can, int tar) {
+         vector<vector<int>> ans;
+         vector<int> v;
+         int n=can.size();
+sub(ans,v,n,0,tar,can);
+return ans;
     }
 };
