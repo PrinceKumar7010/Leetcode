7d16db7d9b98aa582e9fec769389ab601d9cafc8
Prince Kumar <115650778+PrinceKumar7010@users.noreply.github.com>
2024-02-25 12:58:21 +0530

    Time: 59 ms (43.05%) | Memory: 51.8 MB (9.56%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    int coinChange(vector<int> &coins, int amount)
-    {
+    int count(vector<int>& coins, int amount, int i, vector<vector<int>>& dp) {
 
-        int value[10001];
-        value[0] = 0;
-        for (int x = 1; x <= amount; x++)
-        {
-            value[x] = 1e5;
-            for (auto c : coins)
-            {
-                if (x - c >= 0)
-                {
-                    value[x] = min(value[x], value[x - c] + 1);
-                }
-            }
+        if (amount == 0)
+            return 0;
+        if (i < 0)
+            return 1e9;
+        if (dp[i][amount] != -1)
+            return dp[i][amount];
+        int take = 1e9;
+        if (amount >= coins[i]) {
+            take = count(coins, amount - coins[i], i, dp) + 1;
         }
+        int ntake = count(coins, amount, i - 1, dp);
 
-        return (value[amount] == 1e5)?-1:value[amount];
+        return dp[i][amount] = min(take, ntake);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        int u=count(coins, amount, n - 1, dp);
+         if(u==1e9)return -1;
+        return u;
     }
 };
